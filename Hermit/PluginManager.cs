e4b1712f686964888d9ae4c8d39b9830078e2b6a
Lucas Brendel <lucasbrendel@gmail.com>
2014-12-27 20:55:39 -0600

    Plugin Manager and Settings Window
    Added a class for retrieving installed 'Plugins'. Created settings
    window that loads settings items in a seperate window.

---
+++ Hermit/PluginManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel.Composition;
+using System.ComponentModel.Composition.Primitives;
+using System.ComponentModel.Composition.Hosting;
+using System.Reflection;
+using Hermit.Plugin;
+
+namespace Hermit
+{
+    public class PluginManager
+    {
+        [ImportMany]
+        private IList<IPlugin> plugins;
+
+        public PluginManager()
+        {
+            plugins = new List<IPlugin>();
+
+            var catalog = new AssemblyCatalog(Assembly.GetExecutingAssembly());
+            var container = new CompositionContainer(catalog);
+            container.ComposeParts(this);
+        }
+
+        public IList<T> GetPlugins<T>() where T : IPlugin
+        {
+            return plugins.OfType<T>().ToList<T>();
+        }
+    }
+}
---
+++ Hermit/SettingsViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Caliburn.Micro;
+using Hermit.Plugin;
+
+namespace Hermit
+{
+    public class SettingsViewModel : Screen
+    {
+        private PluginManager plugins;
+        private List<ISettingsItem> _Settings = new List<ISettingsItem>();
+
+        public List<ISettingsItem> Settings
+        {
+            get { return _Settings; }
+            set
+            {
+                _Settings = value;
+            }
+        }
+
+        public SettingsViewModel()
+        {
+            this.DisplayName = "Settings";
+            plugins = new PluginManager();
+            _Settings = plugins.GetPlugins<ISettingsItem>().ToList<ISettingsItem>();
+        }
+
+    }
+}
